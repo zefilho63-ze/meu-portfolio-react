28cf79fc0315f9b28c8412de13d8016c6c08dd04
zefilho63-ze <ze.filho63@hotmail.com>
2026-04-16 18:11:24 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,4 +3,13 @@ git commit -m "Meu projeto em React"
 git push origin main
 git add .
 git commit -m "Meu projeto em React"
-git push origin main
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Meu Portfólio React</h1>
+      <p>Projeto carregado com sucesso!</p>
+    </div>
+  );
+}
+
+export default App;
